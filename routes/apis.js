5d4fb558e224742cc7ef5f504bf94a82c3824b25
kerrycc <kerrycc.wu@gmail.com>
2017-08-11 13:31:02 +0800

    Test

--- routes/apis.js
+++ routes/apis.js
@@ -256,35 +256,49 @@ router.get('/test/insert1', function(req, res, next) {
 });
 
 router.get('/test/read/:id', function(req, res, next) {
-
     var config1={
-        user:'kerry',
+        userName:'kerry',
         password:'asdfQWER1234',
-        server:'pvservice.database.windows.net',   //這邊要注意一下!!
-        database:'PVDB',
+        server:'servercp.database.windows.net',   //這邊要注意一下!!
+        //database:'PVCPS',
         options: {
-            encrypt: true // Use this if you're on Windows Azure
+            encrypt: true, // Use this if you're on Windows Azure
+            database:'PVCPS'
         }
     };
 
-    var conn = new sql.ConnectionPool(config1);
-    //connect to your database
-    conn.connect().then(function(){
-        var request = new sql.Request(conn)
-        request.query('select * from Message where itemid=' +  req.params.id + ' and isActive = 1').then(function(result){
-            if (result.recordset.length > 0){
-                res.send(result.recordset[0]);
-            }else{
-                res.send(null);
-            }
-        }).catch(function(err) {
-            console.log('Request error: ' + err);
-        }).then(function(){
-            conn.close();
+    var Connection  = require('tedious').Connection;
+    var Request = require('tedious').Request;
+
+
+    //
+    var connection = new Connection(config1);
+    connection.on('connect', function(err) {
+        if (err) return console.error(err);
+        console.log("Connected");
+        var request = new Request('select * from Message where itemid=' +  req.params.id + ' and isActive = 1', function(err) {
+            if (err) {
+                console.log(err);}
         });
-    }).catch(function(err){
-        console.log(err);
+        var result = "";
+        request.on('row', function(columns) {
+            columns.forEach(function(column) {
+                if (column.value === null) {
+                    console.log('NULL');
+                } else {
+                    result+= column.value + " ";
+                }
+            });
+            res.send(result);
+        });
+
+        request.on('done', function(rowCount, more) {
+            console.log(rowCount + ' rows returned');
+        });
+        connection.execSql(request);
     });
+
+    var a = 0;
 
     //res.render('index', { title: 'Express' });
 });
